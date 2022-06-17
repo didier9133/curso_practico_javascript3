a31cadf20a85e84e13f4fecef014bc4f461b9ba0
Didier Contreras <didier9133@gmail.com>
2022-06-17 13:37:14 -0400

    funciones de javascript para el calculo de las medias propuestas en el reto #3

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # curso_practico_javascript3
+
+...
+
+## Taller #3: promedio, mediana y moda
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las formulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
---
+++ media_geo_armo.js
@@ -0,0 +1,74 @@
+//La media geométrica se calcula como un producto conjunto. 
+//Es decir, que todos los valores se multiplican entre sí.
+//Uno de sus principales usos es para calcular medias sobre porcentajes, 
+//pues su cálculo ofrece unos resultados más adaptados a la realidad
+
+
+function calculando_media_geometrica(array){
+    
+    array.sort(function(a,b){return a -b})
+    
+    const sumalista=array.reduce(
+
+        function (valor_acumulado,nuevo_elemento){
+            return valor_acumulado * nuevo_elemento;
+        }
+    );
+    
+    const media_geometrica = (sumalista)**1/array.length;
+    
+    
+    return media_geometrica;
+}
+
+
+const lista=[
+    1.20,
+    1.33,
+    1.15,
+    1.25,
+    
+]
+
+
+// la media armónica es una medida estadística recíproca a la media aritmética, 
+//que es la suma de un conjunto de valores entre el número de observaciones.
+//Este tipo de media suele utilizarse, principalmente, en velocidades, tiempos, o en el área de la electrónica.
+// Sin embargo, su uso no está muy extendido en otras disciplinas.
+
+
+function array_invertido(elemento){
+    return 1/elemento
+}
+
+const lista_original=[
+    15,
+    14,
+    17,
+    13,
+    12
+    
+];
+
+const nueva_lista=lista_original.map(array_invertido);
+
+function calculando_media_armonica(array){
+    
+    array.sort(function(a,b){return a -b})
+    
+    const sumalista=array.reduce(
+
+        function (valor_acumulado=0,nuevo_elemento){
+            return valor_acumulado + nuevo_elemento;
+        }
+    );
+    
+    const media_armonica=array.length/sumalista;
+
+    console.log(sumalista);
+    return media_armonica;
+}
+
+
+
+
